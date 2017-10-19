142fe5580115b2a92ab4dbbcc188ee25474e10bd
Fritz Arnold <farnoldium@gmail.com>
2017-10-19 11:54:29 +0200

    reworking and commenting

--- grab_weather_data.py
+++ grab_weather_data.py
@@ -1,3 +1,7 @@
+"""This script downloads and processes weather data from the ftp server of the German national weather service.
+Total runtime will be approximately 36 hours. Final data will be saved in numpy binary files. Total size of the 
+final data will be around 10 GB. Only data for 2015 is downloaded."""
+
 import iris
 import datetime as dt
 import time
@@ -9,84 +13,94 @@ import os
 import bz2
 import urllib
 
-variables = ['T_2M']
-#['SWDIFDS_RAD', 'SWDIRS_RAD', 'V_10M', 'U_10M', 'TOT_PRECIP', 'T_2M']
+# define variables
+variables = ['SWDIFDS_RAD', 'SWDIRS_RAD', 'V_10M', 'U_10M', 'TOT_PRECIP', 'T_2M']
 
+# define lat-lon grid to keep (these particular coordinates are chosen to span Germany)
 ll_lat = 365
 ur_lat = 510
 ll_lon = 375
 ur_lon = 485
 
+# download directory
 raw_out = './raw_data/grib/'
 
-max_values = dict([(var, 0) for var in variables])
-
+# path to ftp server
 base_path = 'ftp://ftp-cdc.dwd.de/pub/REA/COSMO_REA6/hourly/2D/'
-for var in variables:
-    if var == 'TOT_PRECIP':
-        mrange = range(9, 13, 1)
-    else:
-        mrange = range(7, 13, 1)
 
+# range of month to download
+mrange = range(1, 13, 1)
+
+# iterate over variables and months 
+for var in variables:
     for month in mrange:
         
         arr = []
         
+        # format string representation of month
         if month >= 10:
             month_string = str(month)
         else:
             month_string = '0' + str(month)
-            
+        
+        # create a file to write timeseries index of downloaded data to
         time_index = open('./processed_data/weather/{}.2D.2015{}.txt'.format(var, month_string), 'w')
+       
         path = base_path + var + '/' + '{}.2D.2015{}.grb.bz2'.format(var, month_string)
         
         print(path)
         
+        # download data file
         _ = wget.download(path, out=raw_out)
         
         filename = raw_out + '{}.2D.2015{}.grb.bz2'.format(var, month_string)
         
         print('download finished')
         
-        zipfile = bz2.BZ2File(filename) # open the file
-        data = zipfile.read() # get the decompressed data
-        newfilepath = filename[:-4] # new file name
+        # open, decompress, save decompressed
+        zipfile = bz2.BZ2File(filename) 
+        data = zipfile.read()
+        newfilepath = filename[:-4]
         
         with open(newfilepath, 'wb') as f:
             f.write(data)
         
         print('unzip finished')
         
-        cubes = iris.load_cube(newfilepath)
+        # load decompressed data into iris.cubes object for manipulation
         
+        # erroneous data for this var-month combination, contains multiple cubes
+        # keep last cube (assumption: last cube == best data)
+        if var == 'TOT_PRECIP' and month == 8:
+            cubes = iris.load(newfilepath)[1]
+        else:
+            cubes = iris.load_cube(newfilepath)
+        
+        # extract data from cubes by iterating over data-structure
         for i in range(cubes.shape[0]):
+            
+            # one entry represents one hour of the respective month
             print('cube {}'.format(i))
+            
+            # get timestamp in gmt format
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(cubes[i].coords('time')[0].cell(0).point.round()*3600))
+            
+            # extract weather data for the predefined grid as numpy array
             data = cubes[i].data[ll_lat:ur_lat, ll_lon:ur_lon]
-#             print(data)
+
             arr.append(data)
             time_index.write(timestamp + '\n')
-            max_value = np.amax(data)
-            if max_value > max_values[var]:
-                max_values[var] = max_value
-        
+
+
         print('data extracted')
-        
         del(cubes)
     
+        # save data to npy file
         time_index.close()
         arr = np.array(arr)
         np.save('./processed_data/weather/{}.2D.2015{}.npy'.format(var, month_string), arr)
         
+        # clean up
         os.remove(filename)
         os.remove(newfilepath)
         urllib.urlcleanup()
-        
-        
-with open('./processed_data/weather/max_values.json', 'r') as f:
-	json.dump(max_values, f)
-#json.dump(max_values, './processed_data/weather/max_values.json')
-        
-# 'SWDIRS_RAD/SWDIRS_RAD.2D.201506.grb.bz2''
-
-# ftp://ftp-cdc.dwd.de/pub/REA/COSMO_REA6/hourly/2D/SWDIRS_RAD/SWDIRS_RAD.2D.201508.grb.bz2
--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+"""Functions used for multi-dimensional weather data manipulation"""
+
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -7,6 +9,10 @@ from functools import reduce
 
 def prepare_training_data(months=range(1, 13, 1), inputs=['SWDIFDS_RAD', 'SWDIRS_RAD'], return_index=True, 
                          base_path='./processed_data/weather', scale=True):
+    
+    """Load specified weather variables and their respective timeseries indices from .npy/.txt files. Scale
+    each variable to 0-1 range by dividing by the maximum observed value. Return final data tensors and indices
+    as dictonary with key=variable_name. Each tensor will contain all data samples for a specific variable."""
     
     indices = {}
     tensors = {}
@@ -19,24 +25,33 @@ def prepare_training_data(months=range(1, 13, 1), inputs=['SWDIFDS_RAD', 'SWDIRS
 
         for month in months:
             
+            # define filenames
             date = dt.date(2015, month, 1).strftime('%Y%m')
             filename = os.path.join(base_path, '{}.2D.{}'.format(variable, date))
             
+            # load index of datafile from .txt file
             index_arr.append(pd.read_csv('{}.txt'.format(filename), parse_dates=True, header=None, index_col=0))
             
+            # load data for current month
             data = np.load('{}.npy'.format(filename))
             
+            # get maximum value
             max_val = np.amax(data)
             
+            # check if new maximum value was found
             if scale:
                 amax = max(amax, max_val)
             
+            # append month data to current variable tensor
             data_arr.append(data)
         
+        # scale data by global maximum of current variable
         if scale:
             data_arr /= amax
 
         indices[variable] = pd.concat(index_arr).index
+        
+        # form one big array from monthly subsets
         tensors[variable] = np.vstack(data_arr)
 
     if return_index:
@@ -47,10 +62,15 @@ def prepare_training_data(months=range(1, 13, 1), inputs=['SWDIFDS_RAD', 'SWDIRS
 
 def check_indices(indices):
     
+    """Calculate the union of all provided indices.
+    Check if any index has missing values compared to that union."""
+    
     missing = dict([(key, None) for key in indices.keys()])
     
+    # caclulate union of indices
     common_index = reduce(lambda  left,right: left.join(right, how='outer'), indices.values())
     
+    # check if union contains entries that are not present in a particular index
     for key, val in indices.items():
         missing[key] = common_index.difference(val)
         
@@ -59,13 +79,27 @@ def check_indices(indices):
 
 def align_transform_data(labels, feature_dict, variables, scaler, index):
     
+    """Clean feature arrays of samples that are missing in a given set of labels. Scale
+    label data. Stack all features in one feature array by merging on third dimension."""
+    
+    assert all((len(labels) == len(feature) for feature in feature_dict.values()))
+    
+    # find NaN values in label dataset
     mask = labels.isnull().values
+    
+    # drop NaN entries in label dataset
     labels.dropna(inplace=True)
     
+    # make index of integer positions of labels that are not missing
     not_missing = np.array([pos for pos, _ in enumerate(~mask) if _ ])
+    
+    # transform label data
     labels = scaler.fit_transform(labels)
     
+    # stack all features into one array 
     stacked_features = np.stack((feature_dict[var] for var in variables), axis=-1)
+    
+    # only keep samples that are not missing in the label
     stacked_features = stacked_features[not_missing]
     
     print('features shape: {}'.format(stacked_features.shape))
@@ -76,16 +110,23 @@ def align_transform_data(labels, feature_dict, variables, scaler, index):
 
 def daily_train_valid_test_split(X, y, index, ret_test_index=True, test_size=.15, valid_size=.15, random_state=7):
     
+    """Split data into train, test and validation set while keeping all hours of one day within one set. Data is only
+    splitted in daily chunks."""
+    
     assert len(X) == len(y) == len(index), 'Input dimensions not matching'
     
+    # transform %-set sizes to appropriate 24 hour chunks
     len_test = int(len(X) // (1 / test_size) // 24)
     len_valid = int(len(X) // (1 / valid_size) // 24)
-        
+    
+    # get integer representation of available days in the dataset
     available_days = index.map(lambda t: t.date()).unique()
     
+    # randomly pick days for test and validation set
     rnd = np.random.RandomState(random_state)
     indices = rnd.choice(available_days, size=len_test+len_valid)
-        
+    
+    # slice data into subsets by getting all hour-entries for the respective days chosen for that subset
     ix_test = [i for i, date in enumerate(index.date) if date in indices[:len_test]]
     ix_valid = [i for i, date in enumerate(index.date) if date in indices[len_test:]]
     ix_train = [i for i, date in enumerate(index.date) if not date in indices]
